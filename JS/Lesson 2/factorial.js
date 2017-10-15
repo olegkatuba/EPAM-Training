ac233ce64b54af07576606bb173c4263aefe36ad
olegkatuba <olegkatuba@gmail.com>
2017-10-15 22:06:41 +0300

    Lesson 3 complited.

--- JS/Lesson 2/factorial.js
+++ JS/Lesson 2/factorial.js
@@ -1,9 +1,10 @@
 var a = parseInt(prompt("Number to calculate factorial", "10"));
 
 var fact = 1;
-if (a === a && a >= 0)
+if (a === a && a >= 0) {
     for (var i = 2; i <= a; i++)
         fact *= i;
+    document.write("Factorial of ", a, " is ", fact);
+}
 else
-    fact = "indefined";
-document.write("Factorial of ", a, " is ", fact);
+    document.write("Factorial of ", a, " is indefined");
